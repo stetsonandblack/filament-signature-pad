bff2a961b5f3bcff7a576102cd36c0e14f5f126d
Pierre Mary <pierre_mary@live.ca>
2025-02-01 18:55:52 -0500

    Fix

--- resources/dist/components/signature-pad.js
+++ resources/dist/components/signature-pad.js
@@ -393,7 +393,7 @@ function E(g, t) {
             })
         },
         save() {
-            this.state = this.signaturePad.toDataURL("image/svg+xml"), this.resizeCanvas()
+            this.state = this.signaturePad.toDataURL("image/jpeg"), this.resizeCanvas()
         },
         clear() {
             this.signaturePad.clear(), this.state = null, this.resizeCanvas()
